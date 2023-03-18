4e501dcfe1b85de32272f1cb9bd1a603836e06ee
Javier Martinez <erick97am@gmail.com>
2023-03-18 12:18:57 -0600

    nvim: Added tree-sitter plugin
    nvim requires the tree-sitter package on ArchLinux so might as well use
    it

--- nvim/lua/config/fold.lua
+++ nvim/lua/config/fold.lua
@@ -1,9 +1,9 @@
-vim.opt.foldmethod = 'indent'
--- vim.opt.foldenable = false
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.foldenable = false
 vim.opt.foldcolumn = "auto:2"
 vim.opt.foldminlines = 5
 vim.opt.fillchars = {
     fold = " "
 }
 vim.opt.foldlevel = 5
--- vim.cmd("set foldlevel=99")
---
+++ nvim/lua/plugins/tree-sitter.lua
@@ -0,0 +1,26 @@
+return {
+    {
+        'nvim-treesitter/nvim-treesitter',
+        build = ":TSUpdate",
+        config = function ()
+            require'nvim-treesitter.configs'.setup {
+                ensure_installed = { 'lua', 'rust', 'bash' },
+                sync_install = true,
+                auto_install = true,
+                highlight = {
+                    enable = true,
+                    additional_vim_regex_highlighting = false
+                },
+                incremental_selection = {
+                    enable = true,
+                    keymaps = {
+                        init_selection = "<CR>",
+                        scope_incremental = "<CR>",
+                        node_incremental = "<TAB>",
+                        node_decremental = "<S-TAB>",
+                    }
+                }
+            }
+        end
+    }
+}

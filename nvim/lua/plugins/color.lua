8beb1a5312a133bf6108c5ef077877696a7d4c15
Javier Martinez <erick97am@gmail.com>
2023-05-04 19:46:21 -0600

    nvim: Better colors

--- nvim/lua/plugins/color.lua
+++ nvim/lua/plugins/color.lua
@@ -1,19 +1,33 @@
 return {
     {
-        'ellisonleao/gruvbox.nvim',
+        'rebelot/kanagawa.nvim',
         lazy = false,
         priority = 1000,
         config = function()
             if vim.fn.has("termguicolors") then
                 vim.opt.termguicolors = true
             end
-            vim.o.background = "dark"
-            require("gruvbox").setup({
-                contrast = "hard",
-                dim_inactive = true,
-                transparent_mode = true
+            require("kanagawa").setup({
+                undercurl = false,
+                dimInactive = true,
             })
-            vim.cmd("colorscheme gruvbox")
+            vim.cmd("colorscheme kanagawa-wave")
         end,
-    }
+    },
+    {
+        'rose-pine/neovim',
+        name = "rose-pine",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            if vim.fn.has("termguicolors") then
+                vim.opt.termguicolors = true
+            end
+            require("rose-pine").setup({
+                variant = 'moon',
+                dim_nc_background = true
+            })
+            -- vim.cmd("colorscheme rose-pine")
+        end,
+    },
 }

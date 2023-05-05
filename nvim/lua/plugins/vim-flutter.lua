cdc95b73092a14c22e3ec39b0deade2104e95e85
Javier Martinez <erick97am@gmail.com>
2023-05-04 19:44:33 -0600

    nvim: Flutter support

---
+++ nvim/lua/plugins/vim-flutter.lua
@@ -0,0 +1,12 @@
+return {
+    {
+        'thosakwe/vim-flutter',
+        lazy = true,
+        event = {
+            'BufEnter *.dart'
+        },
+        ft = {
+            "dart"
+        }
+    }
+}

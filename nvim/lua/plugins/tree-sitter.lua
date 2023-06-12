877af32088477bb4bdac9b2dd5a20f87f787d6f4
Javier Acosta <erick97am@gmail.com>
2023-06-12 12:22:04 -0600

    nvim: Disable treesitter highlighting for gitcommit

--- nvim/lua/plugins/tree-sitter.lua
+++ nvim/lua/plugins/tree-sitter.lua
@@ -1,26 +1,27 @@
 return {
-    {
-        'nvim-treesitter/nvim-treesitter',
-        build = ":TSUpdate",
-        config = function ()
-            require'nvim-treesitter.configs'.setup {
-                ensure_installed = { 'lua', 'rust', 'bash' },
-                sync_install = false,
-                auto_install = true,
-                highlight = {
-                    enable = true,
-                    additional_vim_regex_highlighting = false
-                },
-                incremental_selection = {
-                    enable = true,
-                    keymaps = {
-                        init_selection = "<CR>",
-                        scope_incremental = "<CR>",
-                        node_incremental = "<TAB>",
-                        node_decremental = "<S-TAB>",
-                    }
-                }
-            }
-        end
-    }
+	{
+		'nvim-treesitter/nvim-treesitter',
+		build = ":TSUpdate",
+		config = function()
+			require 'nvim-treesitter.configs'.setup {
+				ensure_installed = { 'lua', 'bash', 'fish' },
+				sync_install = false,
+				auto_install = true,
+				highlight = {
+					enable = true,
+					disable = { "gitcommit" },
+					additional_vim_regex_highlighting = false
+				},
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						init_selection = "<CR>",
+						scope_incremental = "<CR>",
+						node_incremental = "<TAB>",
+						node_decremental = "<S-TAB>",
+					}
+				}
+			}
+		end
+	}
 }

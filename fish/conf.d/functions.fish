7f57c21ea7f4218ee188c93d634a64fe1710c122
Javier Acosta <erick97am@gmail.com>
2023-06-19 08:08:34 -0600

    fish: Latest config

--- fish/conf.d/functions.fish
+++ fish/conf.d/functions.fish
@@ -26,6 +26,14 @@ function dunmount
     udisksctl unmount -b $argv
 end
 
+function mountISO
+	udisksctl loop-setup -r -f $argv
+end
+
+function unmountISO
+	udisksctl loop-delete -b $argv
+end
+
 function audioToMP3
     ffmpeg -i $argv[1] -acodec mp3 $argv[1].mp3
 end
@@ -34,3 +42,6 @@ function restartWlan
     rfkill block wlan && sleep 3s && rfkill unblock wlan && echo "Wifi reiniciado"
 end
 
+function prettyJSON 
+	python -m json.tool $argv
+end

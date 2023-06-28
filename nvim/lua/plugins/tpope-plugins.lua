595aec67267a7e890a3c84323ee479310dcc8957
Javier Martinez <erick97am@gmail.com>
2023-06-28 15:56:07 -0600

    nvim: Added fugitive plugin from tpope

--- nvim/lua/plugins/tpope-plugins.lua
+++ nvim/lua/plugins/tpope-plugins.lua
@@ -1,8 +1,14 @@
 return {
-    {
-        'tpope/vim-commentary',
-    },
-    {
-        'tpope/vim-surround',
-    }
+	{
+		'tpope/vim-commentary',
+	},
+	{
+		'tpope/vim-surround',
+	},
+	{
+		'tpope/vim-fugitive',
+		config = function()
+			vim.keymap.set('n', '<leader>gs', ":Git<CR>")
+		end
+	}
 }

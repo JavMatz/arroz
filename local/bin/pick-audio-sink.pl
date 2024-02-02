926d90306d9045a5b7e07a466b6310c05bd9f678
Javier Martínez <erick97am@gmail.com>
2024-02-02 17:32:11 -0600

    Perl script to change default sink

---
+++ local/bin/pick-audio-sink.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use IPC::Open2;
+
+my $RAW_SINKS = readpipe('pamixer --list-sinks | tail -n 2');
+
+my @SINK_NAMES;
+my %SINKS;
+
+foreach my $SINK (split(/\n/, $RAW_SINKS)) {
+  my @SINKID = split('"', $SINK);
+  push @SINK_NAMES, $SINKID[5];
+  $SINKS{$SINKID[5]} = $SINKID[1];
+}
+
+my ($DMENU_OUT, $DMENU_IN);
+
+my $PID = eval {
+  open2($DMENU_OUT, $DMENU_IN, "bemenu -i");
+} or do {
+  print STDERR "$@";
+  say STDERR "Error";
+  exit 1;
+};
+
+say $DMENU_IN $_ for sort keys %SINKS;
+close($DMENU_IN);
+
+waitpid($PID, 0);
+my $STATUS = ($? >> 8);
+
+exit $STATUS unless $STATUS == 0;
+
+my $CHOICE = <$DMENU_OUT>;
+chomp($CHOICE);
+
+system("pactl", "set-default-sink", "$SINKS{ $CHOICE }");
